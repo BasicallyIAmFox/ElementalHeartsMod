637303fbfcd9a39161280dfe9dafebdbbbb27233
Vincent Jenei <vincentjenei@gmail.com>
2021-11-13 17:14:45 -0800

    Prehardmode a-d finished

--- EHMod.cs
+++ EHMod.cs
@@ -28,10 +28,34 @@ namespace ElementalHeartsMod
         }
         public class PreHardmode
         {
-            public class Amber : EHBase { public Amber() : base(category: "PreHardmode", rarity: 0, station: TileID.TreeAmber, material: ItemID.Amber) { } }
-            public class Amethyst : EHBase { public Amethyst() : base(category: "PreHardmode", rarity: 0, station: TileID.TreeAmethyst, material: ItemID.Amethyst) { } }
-            public class BorealWood : EHBase { public BorealWood() : base(category: "PreHardmode", rarity: 0, station: TileID.Trees, material: ItemID.BorealWood) { } }
-            public class Cactus : EHBase { public Cactus() : base(category: "PreHardmode", rarity: 0, station: TileID.Cactus, material: ItemID.Cactus) { } }
+            public class Amber : EHBase { 
+               public Amber() : base(category: "PreHardmode", rarity: 0, station: TileID.TreeAmber, material: ItemID.Amber) { } }
+            public class Amethyst : EHBase { 
+               public Amethyst() : base(category: "PreHardmode", rarity: 0, station: TileID.TreeAmethyst, material: ItemID.Amethyst) { } }
+            public class BorealWood : EHBase { 
+               public BorealWood() : base(category: "PreHardmode", rarity: 0, station: TileID.Trees, material: ItemID.BorealWood) { } }
+            public class Cactus : EHBase { 
+                public Cactus() : base(category: "PreHardmode", rarity: 0, station: TileID.Cactus, material: ItemID.Cactus) { } }
+            public class CandyCane : EHBase {
+                public CandyCane() : base(category: "PreHardmode", rarity: 0, station: TileID.WorkBenches, material: ItemID.CandyCaneBlock) { } }
+            public class Cloud : EHBase {
+                public Cloud() : base(category: "PreHardmode", rarity: 0, station: TileID.WorkBenches, material: ItemID.Cloud) { } }
+            public class Copper : EHBase { 
+                public Copper() : base(category: "PreHardmode", rarity: 0, station: TileID.Furnaces, material: ItemID.CopperOre) { } }
+            public class Coralstone : EHBase {
+                public Coralstone() : base(category: "PreHardmode", rarity: 0, station: TileID.WorkBenches, material: ItemID.CoralstoneBlock) { } }
+            public class Crimsand : EHBase { 
+                public Crimsand() : base(category: "PreHardmode", rarity: 0, station: TileID.HeavyWorkBench, material: ItemID.CrimsandBlock) { } }
+            public class Crimtane : EHBase {
+                public Crimtane() : base(category: "PreHardmode", rarity: 1, station: TileID.Furnaces, material: ItemID.CrimtaneOre) { } }
+            public class Demonite : EHBase {
+                public Demonite() : base(category: "PreHardmode", rarity: 1, station: TileID.Furnaces, material: ItemID.DemoniteOre) { } }
+            public class Diamond : EHBase { 
+                public Diamond() : base(category: "PreHardmode", rarity: 0, station: TileID.Cactus, material: ItemID.Cactus) { } }
+            public class Dirt : EHBase {
+                public Dirt() : base(category: "PreHardmode", rarity: 0, station: TileID.WorkBenches, material: ItemID.DirtBlock) { } }
+            public class Dynasty : EHBase {
+                public Dynasty() : base(category: "PreHardmode", rarity: 0, station: TileID.Trees, material: ItemID.DynastyWood) { } }
         }
     }
 }
